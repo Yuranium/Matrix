64b2c10266d6f53e47cfd697cea8f9cdfd3be1ab
Yuranium <141080922+Yuranium@users.noreply.github.com>
2023-08-21 01:29:03 +0300

    Update Main.java
    Ввод/вывод матрицы, заполнение в двух режимах, установка диапазона значений в матрице

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,66 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws NumberFormatException
     {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++)
+        int num, size, sizeNum;
+        try (Scanner sc = new Scanner(System.in))
         {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            System.out.print("Введите диапазон чисел в матрице: ");
+            sizeNum = sc.nextInt();
+            if (sizeNum < 0)
+                sizeNum = -sizeNum;
+            do
+            {
+                System.out.print("Введите размер матрицы: ");
+                size = sc.nextInt();
+                if (size > 10 || size < 0)
+                    System.out.println("Ошибка ввода размера");
+            } while (size > 10 || size < 0);
+            int[][] matrix = new int[size][size];
+            do
+            {
+                System.out.println("Заполнение матрицы: 1 - автоматически; 2 - вручную");
+                num = sc.nextInt();
+                if (num > 2 || num <= 0)
+                    System.out.println("Недопустимый диапазон выбора!");
+            } while (num > 2 || num <= 0);
+            switch (num)
+            {
+                case 1:
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                            matrix[i][j] = (int) ((Math.random() * sizeNum) - (sizeNum / 2));
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            do
+                            {
+                                System.out.println("Ввод числа с индексами: [" + i + "][" + j + "]");
+                                matrix[i][j] = sc.nextInt();
+                                if (matrix[i][j] > (sizeNum / 2) || matrix[i][j] < -(sizeNum / 2))
+                                    System.out.println("Число вне диапазона! Введите число еще раз");
+                            } while (matrix[i][j] > (sizeNum / 2) || matrix[i][j] < -(sizeNum / 2));
+                        }
+                    }
+            }
+            System.out.println("Заполненная матрица:");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                    System.out.print(matrix[i][j] + "  ");
+                System.out.println();
+            }
+        }
+        catch (InputMismatchException exc)
+        {
+            System.out.println("Ошибка введённого типа, " + exc);
         }
     }
 }
